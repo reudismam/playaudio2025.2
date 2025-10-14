4585ca7a239b77b8b44a4140860775179d6c1145
reudismam <reudismam@gmail.com>
2025-10-14 17:10:55 -0300

    Aula 14-10

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,31 +1,77 @@
 'use client'
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
-
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa6";
 export default function Play() {
-
-  let [contador, setContador] = useState<number>(0);
+  const [audio, setAudio] = useState<HTMLAudioElement>();
+  const [playing, isPlaying] = useState<boolean>(false);
+  const [gain, setGain] = useState<GainNode>();
+  const [volume, setVolume] = useState<number>(1);
 
   useEffect(()=>{
-      alert(contador);
-  }, [contador]);
+    configAudio("./audios/audio1.mp3");
+  }, []);
 
-  const increment = () => {
-    setContador(contador + 1);
-    console.log(contador);
+  const configVolume = (newValue: number) => {
+    if (gain) {
+      gain.gain.value = newValue;
+    }
+    setVolume(newValue);
   }
   
-  function decrement() {
-    setContador(contador - 1);
-    console.log(contador);
+  const configAudio = (url: string) => {
+    const newAudio = new Audio(url);  
+    setAudio(newAudio);
+
+    const audioContext = new AudioContext();
+    const media = audioContext.createMediaElementSource(newAudio);
+    const newGanho = audioContext.createGain();
+    media.connect(newGanho);
+    newGanho.connect(audioContext.destination);
+    setGain(newGanho);
   }
 
+  const playPause = () => {
+    if (playing) {
+      pause();
+    }
+    else {
+      play();
+    }
+    isPlaying(!playing);
+  }
+
+  const play = () => {
+    if (audio) {
+      audio.play();
+    }
+  }
+
+  const pause = () => {
+    if (audio) {
+      audio.pause();
+    }
+  }
   return (
      <div className="flex h-[100vh] flex-col items-center justify-center">
-      <button onClick={e => increment()} className="bg-blue-500 w-[300px]">Incrementar</button>
-      <button onClick={e => decrement()} className="m-4 bg-blue-500 w-[300px]">Decrementar</button>
-      <h1 className="text-center m-4">{contador}</h1>
+      <button onClick={e => playPause()}>
+         {
+          playing ? 
+             <FaPause className="text-blue-400"/>
+             : <FaPlay className="text-blue-400"/>
+         }
+      </button>
+      <div>
+        <input 
+        type="range" 
+         min={0}
+         max={1}
+         value={volume}
+         step={0.001}
+         onChange={e=> configVolume(Number(e.target.value))}
+        />
+      </div>
 
      </div>
   );
